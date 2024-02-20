6b47d9edf12c89fed76b7b243df3f8229c272058
Rafael <rafaelcrd.ribeiro@gmail.com>
2024-02-20 16:56:02 -0300

    feat: textos Esqueceu a senha? e Primeira vez aqui? adicionados

--- tripbook_app/lib/pages/login.dart
+++ tripbook_app/lib/pages/login.dart
@@ -17,17 +17,26 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 70.0),
           child: SizedBox(
-            height: 300,
             child: Form(
               key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  _buildLogo(context),
-                  _buildFormFields(),
-                  _buildLoginButton(),
+                  const SizedBox(),
+                  SizedBox(
+                    height: 350,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildLogo(context),
+                        _buildFormFields(),
+                        _buildLoginButton(),
+                      ],
+                    ),
+                  ),
+                  _buildRegisterButton(context),
                 ],
               ),
             ),
@@ -97,16 +106,91 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _buildLoginButton() {
-    return Button(
-      label: 'Entrar',
-      onPressed: () {
-        if (_formKey.currentState!.validate()) {
-          // Process data.
-          print('Validação bem-sucedida');
-        } else {
-          print('Falha na validação');
-        }
-      },
+    return Column(
+      children: [
+        Button(
+          label: 'Entrar',
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              // Process data.
+              print('Validação bem-sucedida');
+            } else {
+              print('Falha na validação');
+            }
+          },
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: _buildForgotPassword(context),
+        )
+      ],
     );
+  }
+
+  Widget _buildForgotPassword(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 200,
+                color: Colors.amber,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('BottomSheet de recuperação de senha'),
+                      ElevatedButton(
+                        child: const Text('Fechar BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: Text(
+          'Esqueceu a senha? 🤔',
+          style: Theme.of(context).textTheme.titleSmall,
+        ));
+  }
+
+  Widget _buildRegisterButton(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: 200,
+                color: Colors.amber,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text('BottomSheet de cadastro'),
+                      ElevatedButton(
+                        child: const Text('Fechar BottomSheet'),
+                        onPressed: () => Navigator.pop(context),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        child: Text(
+          'Primeira vez aqui? ✋',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium
+              ?.copyWith(fontWeight: FontWeight.bold),
+        ));
   }
 }
